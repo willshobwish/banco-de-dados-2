4c87ca728a47bdb39e3c54cf10809ffda223020c
Willian Murayama <willianyos13@gmail.com>
2024-08-29 13:49:16 -0300

    Feat: generate text and segmentation

--- backend/inference/inference_models/vit_description.py
+++ backend/inference/inference_models/vit_description.py
@@ -10,13 +10,14 @@ tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-max_length = 200
-num_beams = 50
+max_length = 500
+num_beams = 10
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-def predict_step(image_paths):
+def predict_step(image_folder:str,text_path:str):
   images = []
-  for image_path in image_paths:
+  for image_path in os.listdir(image_folder):
+    image_path = os.path.join(image_folder,image_path)
     i_image = Image.open(image_path)
     if i_image.mode != "RGB":
       i_image = i_image.convert(mode="RGB")
@@ -30,7 +31,12 @@ def predict_step(image_paths):
 
   preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
   preds = [pred.strip() for pred in preds]
-  return preds
+  with open(text_path, "w") as file:
+    # Iterate over the array and write each item on a new line
+    for image_name, prediction in zip(os.listdir(image_folder),preds):
+        file.write(f"{image_name}: {prediction}\n")
 
 
-[print(i) for i in predict_step([os.path.join(r'output_segments',x) for x in os.listdir(r'output_segments')])]
+
+# [print(i) for i in predict_step([os.path.join(r'output_segments',x) for x in os.listdir(r'output_segments')])]
+predict_step(r'output_segments','test.txt')
